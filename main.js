77432ed6d03d2dd50f884d61c568418cf079d66f
André Sampaio <108940285+Decok550@users.noreply.github.com>
2024-06-14 08:55:16 -0300

    14/06

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const contador = document.querySelectorAll(".contador")
+contador[0].textContent="Olá"
+const tempoObjetivo1= new Date (2024,6,12,10,00)
+const tempoObjetivo2= new Date (2024,7,13,11,00)
+const tempoObjetivo3= new Date (2024,8,14,12,00)
+const tempoObjetivo4= new Date (2024,9,15,13,00)
+
 for(let i = 0 ; i < botoes.length; i++){
     botoes[i].onclick = function(){
      for (let j = 0; j< botoes.length ; j++){

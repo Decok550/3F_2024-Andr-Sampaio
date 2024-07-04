5f588e0ce3e7a21779b0896926b75b96863360c0
André Sampaio <108940285+Decok550@users.noreply.github.com>
2024-07-04 09:45:30 -0300

    04/07

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let segundos
 let minutos
 let horas
 let dias 
-segundos= (tempoObjetivo1-agora)/1000
+segundos= (tempoObjetivo-agora)/1000
 minutos= segundos/60 
 horas= minutos/60 
 dias= horas/24 
@@ -22,13 +22,19 @@ dias= Math.floor(dias);
 segundos= segundos%60;
 minutos= minutos%60;
 horas= horas%24;
+return[dias,horas,minutos,segundos]
 }
+let contadorPrimeiro = calculaTempo(tempoObjetivo1)
+contador[0].textContent = `Faltam ${contadorPrimeiro[0]}dias, ${contadorPrimeiro[1]}horas, ${contadorPrimeiro[2]}minutos, ${contadorPrimeiro[3]}segundos.`
+let contadorSegundo = calculaTempo(tempoObjetivo2)
+contador[1].textContent = `Faltam ${contadorSegundo[0]}dias, ${contadorSegundo[1]}horas, ${contadorSegundo[2]}minutos, ${contadorSegundo[3]}segundos.`
+let contadorTerceiro = calculaTempo(tempoObjetivo3)
+contador[2].textContent = `Faltam ${contadorTerceiro[0]}dias, ${contadorTerceiro[1]}horas, ${contadorTerceiro[2]}minutos, ${contadorTerceiro[3]}segundos.`
+let contadorQuarto = calculaTempo(tempoObjetivo4)
+contador[3].textContent = `Faltam ${contadorQuarto[0]}dias, ${contadorQuarto[1]}horas, ${contadorQuarto[2]}minutos, ${contadorQuarto[3]}segundos.`
 
 
-contador[0].textContent= `faltam ${dias} dias,${horas} horas, ${minutos} minutos, ${segundos} segundos`
-contador[1].textContent= tempoObjetivo2-agora
-contador[2].textContent= tempoObjetivo3-agora
-contador[3].textContent= tempoObjetivo4-agora
+
 for(let i = 0 ; i < botoes.length; i++){
     botoes[i].onclick = function(){
      for (let j = 0; j< botoes.length ; j++){
